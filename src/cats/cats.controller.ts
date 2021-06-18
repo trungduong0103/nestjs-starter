afe53e90d72c40fe351d527928c7713165f7ade9
trungduong0103 <trungduong0103@gmail.com>
2021-06-18 21:51:04 +0700

    test with @Post() decorator

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+
+interface catDto {
+  catName: string;
+  catAge: number;
+}
 
 @Controller('cats-api')
 export class CatsController {
@@ -11,11 +16,17 @@ export class CatsController {
   @Get('/get_a_cat_nest')
   getACat(@Req() request: Request): string {
     console.log(request.originalUrl);
-    return 'This action returns a cat';
+    return 'This action returns a cat.';
   }
 
   @Get('/get_a_cat_express')
   getACatExpress(@Req() req: Request, @Res() res: Response) {
-    return res.status(200).send('This action returns a cat using express');
+    return res.status(200).send('This action returns a cat using express.');
+  }
+
+  @Post('/post_a_cat')
+  postACat(@Body() reqBody: catDto): string {
+    console.log(reqBody.catName);
+    return 'This action posts a cat.';
   }
 }
