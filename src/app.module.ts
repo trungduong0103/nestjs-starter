4975c339411205981e9c8afa02bd5e709d3c7cac
trungduong0103 <trungduong0103@gmail.com>
2021-06-22 21:48:35 +0700

    moved modules to modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { PetsModule } from './pets/pets.module';
-import { SubdomainRoutingModule } from './sub-domain-routing/sub-domain-routing.module';
+import { CatsModule } from './modules/cats/cats.module';
+import { PetsModule } from './modules/pets/pets.module';
+// import { PetsModule } from './pets/pets.module';
+// import { SubdomainRoutingModule } from './sub-domain-routing/sub-domain-routing.module';
 
 @Module({
-  imports: [CatsModule, PetsModule, SubdomainRoutingModule],
+  imports: [CatsModule, PetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/pets/pets.controller.ts
+++ src/modules/pets/pets.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { CatsService } from '../cats/cats.service';
+import { PetsService } from './pets.service';
 
 @Controller('pets-api')
 export class PetsController {
-  constructor(private catsService: CatsService) {}
+  constructor(private petsService: PetsService) {}
 
   @Get('/get_all_pets')
   findAll() {
-    return this.catsService.findAll();
+    return this.petsService.getAllPets();
   }
 }
