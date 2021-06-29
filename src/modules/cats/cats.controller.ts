e3691bcfa24c9509c97190d55c34438c55f3f6c2
trungduong0103 <trungduong0103@gmail.com>
2021-06-29 15:47:04 +0700

    clean up cats.controller.ts

--- src/modules/cats/cats.controller.ts
+++ src/modules/cats/cats.controller.ts
@@ -3,21 +3,15 @@ import {
   Controller,
   Delete,
   Get,
-  Header,
-  HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
   Post,
   Put,
   Query,
-  Redirect,
-  Req,
-  Res,
   UseFilters,
   UsePipes,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
 import { CustomUnauthorizedException } from '../../exceptions';
 import {
   BadRequestExceptionFilter,
@@ -85,88 +79,4 @@ export class CatsController {
   }
 
   // end of example
-
-  @Get('/get_a_cat_nest')
-  getACat(@Req() request: Request): string {
-    return `This action returns a cat from ${request.originalUrl}`;
-  }
-
-  @Get('/get_a_cat_express')
-  getACatExpress(@Res() res: Response) {
-    return res
-      .status(HttpStatus.I_AM_A_TEAPOT)
-      .send('This action returns a cat using express.');
-  }
-
-  @Get('/get_a_cat_with_param/:id/:age')
-  getACatWithParam(@Param('id') id: number, @Param('age') age: number): string {
-    return `This action gets a cat with id: ${id}, and age: ${age}.`;
-  }
-
-  @Get('/get_cat_wildcard/ab*cd')
-  getCatWildCard(): string {
-    return 'This action returns a wildcard cat.';
-  }
-
-  @Get('/redirect')
-  @Redirect('https://notion.so', 301)
-  redirect(): string {
-    return "you won't see this because you are redirected to somewhere else.";
-  }
-
-  @Get('/redirect_controller')
-  @Redirect('/bye', 301)
-  redirectController(): string {
-    return 'redirected to AppController';
-  }
-
-  @Get('/override_redirect_decorator')
-  @Redirect('https://docs.nestjs.com', 302)
-  getDocs(@Query('version') version: string) {
-    if (version && version === '5') {
-      return { url: 'https://docs.nestjs.com/v5/', statusCode: 301 };
-    }
-  }
-
-  @Get('/get_all_cats_async')
-  async getAllCatsAsync(): Promise<CreateCatDto[]> {
-    return [
-      {
-        name: 'Tom',
-        age: 2,
-        breed: 'Russian short-hair',
-      },
-      {
-        name: 'Hugo',
-        age: 3,
-        breed: 'Minx',
-      },
-    ];
-  }
-
-  @Post('/post_a_cat')
-  postACat(
-    @Body() reqBody: CreateCatDto,
-    @Body('catAge') catAge: number,
-    @Body('catName') catName: string,
-    @Param() reqParam,
-    @Query() reqQuery,
-  ): string {
-    const reqBodyString = JSON.stringify(reqBody);
-    const reqParamString = JSON.stringify(reqParam);
-    const reqQueryString = JSON.stringify(reqQuery);
-    return `This action post a cat with reqBody: ${reqBodyString}, reqParam: ${reqParamString}, reqQuery: ${reqQueryString}, cat name: ${catName}, cat age: ${catAge}.`;
-  }
-
-  @Post('/post_anything')
-  @HttpCode(204)
-  return204(): string {
-    return 'This action returns a 204 response.';
-  }
-
-  @Post('/post_header')
-  @Header('Cache-Control', 'none')
-  postHeader(): string {
-    return 'Post action with header.';
-  }
 }
